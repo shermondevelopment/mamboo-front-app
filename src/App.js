a205d8889fc92855a3e392b68e9fe77249ea92ba
shermondevelopment <victorshermon@yahoo.com>
2022-12-12 21:36:33 -0300

    feat: create globalStyl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react'
+
+/** globalStyle */
+import GlobalStyle from './styles/globalStlye';
+
 function App() {
   return (
     <>
       <h1>App</h1>
+      <GlobalStyle />
     </>
   );
 }
---
+++ src/styles/globalStlye.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+
+ * {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+  font-family: 'Popins', sans-serif;
+ }
+
+`
+
+export default GlobalStyle
